2383c304afafb77784d2bcc317ab7cf66abd111a
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-18 22:43:31 +0530

    sec 1 developing

--- src/pages/SectionOne/SectionOne.tsx
+++ src/pages/SectionOne/SectionOne.tsx
@@ -7,12 +7,16 @@ export class SectionOne extends Component {
             <>
                 <section className="bg-gray-900">
                     <div className="flex pl-64 pr-64 pt-36">
-                        <div className="bg-gray-200 flex-1">
 
+                        <div className="flex-1 flex justify-center flex-col">
+                            <h1 className="text-[80px] font-black text-white">CHALLENGE</h1>
+                            <h1 className="text-[80px] font-bold text-white">YOUR <span className="text-primary">LIMITS</span></h1>
                         </div>
+
                         <div className="flex-1">
-                            <img src={MainImg} className="w-[500px]"/>
+                        <img src={MainImg} className="w-[550px]"/>
                         </div>
+
                     </div>
                 </section>
             </>
