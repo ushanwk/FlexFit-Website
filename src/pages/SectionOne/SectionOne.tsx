544940892a819b496ead79521667306c0d04483b
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-19 11:07:23 +0530

    sec 1 developing

--- src/pages/SectionOne/SectionOne.tsx
+++ src/pages/SectionOne/SectionOne.tsx
@@ -1,5 +1,9 @@
 import {Component} from "react";
 import MainImg from '../../images/SectionOne/ImgFront.png'
+import XImg from '../../images/SectionOne/twitter.png'
+import FbImg from '../../images/SectionOne/facebook.png'
+import InsterImg from '../../images/SectionOne/instagram.png'
+import LinkedInImg from '../../images/SectionOne/linkedin.png'
 
 export class SectionOne extends Component {
     render() {
@@ -13,20 +17,24 @@ export class SectionOne extends Component {
                             <h1 className="text-[75px] font-medium text-white -mt-10">YOUR LIMITS</h1>
                             <p className="mt-1 text-gray-400">Modern gym equipped with state-of-the-art fitness machines, free weights, and cardio equipment. Offers diverse classes, experienced trainers, and a vibrant atmosphere for individuals seeking a healthy lifestyle and fitness goals.</p>
 
-                            <div className="w-[280px] mt-14 flex justify-between">
+                            <div className="w-[230px] mt-14 flex justify-between">
                                 <div
-                                    className="w-[50px] h-[50px] rounded-[100%] border-[1px] border-gray-500 hover:bg-primary"></div>
+                                    className="w-[50px] h-[50px] rounded-[100%] border-[1px] border-gray-500 hover:bg-primary flex items-center justify-center">
+                                    <img src={XImg} className="w-[15px]"/></div>
                                 <div
-                                    className="w-[50px] h-[50px] rounded-[100%] border-[1px] border-gray-500 hover:bg-primary"></div>
+                                    className="w-[50px] h-[50px] rounded-[100%] border-[1px] border-gray-500 hover:bg-primary flex items-center justify-center">
+                                    <img src={LinkedInImg} className="w-[15px]"/></div>
                                 <div
-                                    className="w-[50px] h-[50px] rounded-[100%] border-[1px] border-gray-500 hover:bg-primary"></div>
+                                    className="w-[50px] h-[50px] rounded-[100%] border-[1px] border-gray-500 hover:bg-primary flex items-center justify-center">
+                                    <img src={InsterImg} className="w-[15px]"/></div>
                                 <div
-                                    className="w-[50px] h-[50px] rounded-[100%] border-[1px] border-gray-500 hover:bg-primary"></div>
+                                    className="w-[50px] h-[50px] rounded-[100%] border-[1px] border-gray-500 hover:bg-primary flex items-center justify-center">
+                                    <img src={FbImg} className="w-[15px]"/></div>
                             </div>
                         </div>
 
                         <div className="flex-1">
-                        <img src={MainImg} className="w-[550px]"/>
+                            <img src={MainImg} className="w-[550px]"/>
                         </div>
 
                     </div>
