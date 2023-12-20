40ee30eb2f6ebb786ae0bd19302f5218c9eec845
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-20 22:23:30 +0530

    sec 5 developing

--- src/pages/SectionFour/SectionFour.tsx
+++ src/pages/SectionFour/SectionFour.tsx
@@ -1,13 +1,16 @@
 import {Component} from "react";
-import ImgBg from '../../images/SectionFour/ImgBg.jpg'
 
 export class SectionFour extends Component {
     render() {
         return (
             <>
-               <section className="h-[500px]">
-                    <img src={ImgBg}/>
-               </section>
+                <section className="mt-24 pb-32 flex text-center flex-col pl-64 pr-64">
+
+                    <h1 className="text-[30px] font-medium">Why <span className="text-primary">Choose </span>Us
+                    </h1>
+                    <h2 className="text-[15px] text-gray-400">Elevate Your Experience with Us</h2>
+
+                </section>
             </>
         );
     }
