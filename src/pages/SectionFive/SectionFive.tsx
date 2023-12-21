085559d9bfd6b33ee31e1a31217c5c659934ccc0
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-21 12:38:03 +0530

    sec 5 bug fix

--- src/pages/SectionFive/SectionFive.tsx
+++ src/pages/SectionFive/SectionFive.tsx
@@ -11,7 +11,7 @@ export class SectionFive extends Component {
                     </h1>
                     <h1 className="text-[75px] font-bold text-white">PAY NOW</h1>
                     <h1 className="text-[65px] font-normal text-white">Get <span
-                        className="font-bold text-primary">25% </span>Discount</h1>
+                        className="font-bold text-primary text-[80px]">25% </span>Discount</h1>
 
                     <button className="border-[1px] border-primary hover:bg-primary rounded-[5px] mt-12 w-[150px]"><h1
                         className="text-[13px] font-medium p-3 text-white">Join Us</h1></button>
