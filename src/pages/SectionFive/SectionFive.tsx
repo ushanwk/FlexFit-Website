b0cecfe70f6f96cab060e1f510e0a28133b38bc0
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-21 11:28:09 +0530

    sec 6 developing

--- src/pages/SectionFive/SectionFive.tsx
+++ src/pages/SectionFive/SectionFive.tsx
@@ -4,7 +4,7 @@ export class SectionFive extends Component {
     render() {
         return (
             <>
-                <section className="bg-gray-900 mt-24 pb-32 flex text-center flex-col pl-64 pr-64">
+                <section className="bg-gray-900 mt-32 pb-24 flex text-center items-center flex-col pl-64 pr-64">
 
                     <h1 className="text-[40px] font-normal mt-28 text-white">Join <span
                         className="font-bold text-primary">FITNESS CLASSES </span>Today
@@ -13,8 +13,8 @@ export class SectionFive extends Component {
                     <h1 className="text-[65px] font-normal text-white">Get <span
                         className="font-bold text-primary">25% </span>Discount</h1>
 
-                    <button className="border-[1px] border-primary hover:bg-primary rounded-[5px] mt-12 w-[200px]"><h1
-                        className="text-[12px] font-medium p-2 text-white">Know About Us</h1></button>
+                    <button className="border-[1px] border-primary hover:bg-primary rounded-[5px] mt-12 w-[150px]"><h1
+                        className="text-[13px] font-medium p-3 text-white">Join Us</h1></button>
 
                 </section>
             </>
