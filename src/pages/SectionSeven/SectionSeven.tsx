9890b82b2e89d1991590c8d693a80a03a2ca7360
UshanKaushalya <Ushanwk22@gmail.com>
2023-12-21 19:03:15 +0530

    sec 7 developing

--- src/pages/SectionSeven/SectionSeven.tsx
+++ src/pages/SectionSeven/SectionSeven.tsx
@@ -1,12 +1,15 @@
 import {Component} from "react";
 import Quote from '../../images/SectionSeven/ImgQuotesLogo.png'
+import Trn1 from '../../images/SectionSix/Trn1.png'
+import Trn2 from '../../images/SectionSix/Trn2.png'
+import Trn3 from '../../images/SectionSix/Trn3.png'
 
 export class SectionSeven extends Component {
     render() {
         return (
             <>
 
-                <section className="bg-gray-100 pb-20 pt-20 flex pl-64 pr-64">
+                <section className="bg-gray-100 pb-28 pt-32 flex pl-64 pr-64">
 
                     <div className="flex-1 flex flex-col pt-2 pb-2 justify-center">
                         <img src={Quote} className="w-[50px]"/>
@@ -17,8 +20,40 @@ export class SectionSeven extends Component {
 
                     <div className="flex-1 flex flex-col gap-5 justify-center">
                         <div className="h-32 bg-white rounded-[15px] shadow-xl flex justify-center">
-                            <div className="bg-black h-[100%] flex-[30%]"></div>
-                            <div className="bg-gray-400 h-[100%] flex-[70%]"></div>
+                            <div className="h-[100%] flex-[20%] flex items-center pl-5">
+                                <img src={Trn1}
+                                     className="h-[90px] w-[90px] rounded-[100%]"/>
+                            </div>
+                            <div className="h-[100%] flex-[80%] flex-col content-center pl-4 pt-5 pr-4">
+                                <h1 className="font-medium text-primary text-[16px]">Lewis John</h1>
+                                <p className="text-gray-400 text-[13px] pt-1">Exceptional gym! Clean facilities,
+                                    friendly staff, and motivating atmosphere. Achieved great results. Highly recommend
+                                    for a rewarding fitness experience!</p>
+                            </div>
+                        </div>
+                        <div className="h-32 bg-white rounded-[15px] shadow-xl flex justify-center">
+                            <div className="h-[100%] flex-[20%] flex items-center pl-5">
+                                <img src={Trn2}
+                                     className="h-[90px] w-[90px] rounded-[100%]"/>
+                            </div>
+                            <div className="h-[100%] flex-[80%] flex-col content-center pl-4 pt-5 pr-4">
+                                <h1 className="font-medium text-primary text-[16px]">Daniel Abrhum</h1>
+                                <p className="text-gray-400 text-[13px] pt-1">Exceptional gym! Clean facilities,
+                                    friendly staff, and motivating atmosphere. Achieved great results. Highly recommend
+                                    for a rewarding fitness experience!</p>
+                            </div>
+                        </div>
+                        <div className="h-32 bg-white rounded-[15px] shadow-xl flex justify-center">
+                            <div className="h-[100%] flex-[20%] flex items-center pl-5">
+                                <img src={Trn3}
+                                     className="h-[90px] w-[90px] rounded-[100%]"/>
+                            </div>
+                            <div className="h-[100%] flex-[80%] flex-col content-center pl-4 pt-5 pr-4">
+                                <h1 className="font-medium text-primary text-[16px]">Brett Lee</h1>
+                                <p className="text-gray-400 text-[13px] pt-1">Exceptional gym! Clean facilities,
+                                    friendly staff, and motivating atmosphere. Achieved great results. Highly recommend
+                                    for a rewarding fitness experience!</p>
+                            </div>
                         </div>
                     </div>
 
